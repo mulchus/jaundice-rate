a45ffaf62ee560eae6a1aea981a3aa5469a5fe13
mulchus <mulch@list.ru>
2023-12-13 22:37:30 +0300

    Added TimeoutError on big timeout.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import aiohttp
 import anyio
+import asyncio
 
 from adapters.inosmi_ru import sanitize, ArticleNotFound
 from text_tools import calculate_jaundice_rate, split_by_words, pymorphy2
+from async_timeout import timeout
 
 
 TEST_ARTICLES = [
@@ -22,19 +24,24 @@ async def fetch(session, url):
 
 
 async def process_article(session, morph, charged_words, url, title, results):
-    parsing_status = 'Ok'
+    parsing_status = ''
     jaundice_rate, len_words = None, None
     try:
-        html = await fetch(session, url)
-        clean_plaintext = sanitize(html, plaintext=True)
-        words = split_by_words(morph, clean_plaintext)
-        len_words = len(words)
-        jaundice_rate = calculate_jaundice_rate(words, charged_words)
+        async with timeout(.7):
+            html = await fetch(session, url)
+            clean_plaintext = sanitize(html, plaintext=True)
+            words = split_by_words(morph, clean_plaintext)
+            len_words = len(words)
+            jaundice_rate = calculate_jaundice_rate(words, charged_words)
+            parsing_status = 'Ok'
     except aiohttp.client_exceptions.ClientResponseError:
         parsing_status = 'WRONG URL!'
         return
     except ArticleNotFound:
         parsing_status = 'PARSING_ERROR'
+        return
+    except asyncio.TimeoutError:
+        parsing_status = 'TIMEOUT'
         return
     finally:
         results.append(
@@ -52,13 +59,13 @@ async def main():
     title = ''
     results = []
     async with aiohttp.ClientSession() as session:
-        try:
-            async with anyio.create_task_group() as task_group:
-                for url in TEST_ARTICLES:
-                    task_group.start_soon(process_article, session, morph, charged_words, url, title, results)
-        except* Exception as excgroup:
-            for _ in excgroup.exceptions:
-                task_group.cancel_scope.cancel()
+        # try:
+        async with anyio.create_task_group() as task_group:
+            for url in TEST_ARTICLES:
+                task_group.start_soon(process_article, session, morph, charged_words, url, title, results)
+        # except* Exception as excgroup:
+        #     for _ in excgroup.exceptions:
+        #         task_group.cancel_scope.cancel()
 
     for result in results:
         print(result)
