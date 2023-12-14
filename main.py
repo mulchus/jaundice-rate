8dc7acf96208ed25c7fbc28e6b1468506bdd5e9c
mulchus <mulch@list.ru>
2023-12-14 16:53:12 +0300

    Added timer.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import anyio
 import asyncio
 import logging
 import time
+import sys
 
 from adapters.inosmi_ru import sanitize, ArticleNotFound
 from text_tools import calculate_jaundice_rate, split_by_words, pymorphy2
@@ -18,6 +19,7 @@ TEST_ARTICLES = [
     'https://inosmi.ru/20231212/ssha-267035917.html',
     'https://lenta.ru/brief/2021/08/26/afg_terror/',
     'https://inosmi.ru/20231212/ssha-267035917.html--',
+    'https://dvmn.org/filer/canonical/1561832205/162/',
 ]
 
 
@@ -44,37 +46,33 @@ async def fetch(session, url):
 
 
 class Timer:
-    def __enter__(self):
-        self.start_time = time.time()
+    def __int__(self):
+        self.elapsed_time = None
         return self
-    
+
+    def __enter__(self):
+        self.start_time = monotonic()
+        return self
+
     def __exit__(self, exc_type, exc_val, exc_tb):
-        elapsed_time = time.time() - self.start_time
-        return elapsed_time
-        # return elapsed_time
-        # print(f"Elapsed time: {elapsed_time} seconds")
+        self.elapsed_time = monotonic() - self.start_time
+        return self
 
 
-async def process_article(session, morph, charged_words, url, title, results):
+async def process_article(session, morph, charged_words, url, title):
     parsing_status = ''
-    # time_delta = 0
+    time_delta = 0
     jaundice_rate, len_words = None, None
     try:
         async with timeout(10):
             html = await fetch(session, url)
-            # start_time = monotonic()
-            
-            # with counter() as time_delta:
             with Timer() as timer:
                 clean_plaintext = sanitize(html, plaintext=True)
                 words = split_by_words(morph, clean_plaintext)
                 jaundice_rate = calculate_jaundice_rate(words, charged_words)
-                time_delta = timer
-                # time.sleep(2)
-                print(time_delta.)
-                # time_delta = monotonic() - start_time
                 len_words = len(words)
                 parsing_status = 'Ok'
+            time_delta = timer.elapsed_time
     except aiohttp.client_exceptions.ClientResponseError:
         parsing_status = 'WRONG URL!'
         return
@@ -85,28 +83,26 @@ async def process_article(session, morph, charged_words, url, title, results):
         parsing_status = 'TIMEOUT'
         return
     finally:
-        results.append(
+        print(
             f'URL: {url}\n'
             f'Статус: {parsing_status}\n'
             f'Рейтинг: {jaundice_rate}\n'
-            f'Слов в статье: {len_words}\n'
+            f'Слов в статье: {len_words}'
         )
-        # if time_delta.:
-        # results.append(
-        #     time_logger.info(f'Анализ закончен за {time_delta} сек')
-        # )
+        time_logger.info(f'Анализ закончен за {round(time_delta, 3)} сек'),
+        print()
 
 
 def configuring_logging():
     time_logger.setLevel(logging.INFO)
-    logger_handler = logging.StreamHandler()
+    logger_handler = logging.StreamHandler(sys.stdout)
     logger_formatter = logging.Formatter(
-        '%(levelname)s:%(message)s',
+        '%(levelname)s:%(name)s:%(message)s',
         datefmt='%d-%m-%Y %H:%M:%S'
     )
     logger_handler.setFormatter(logger_formatter)
     time_logger.addHandler(logger_handler)
-    
+
 
 async def main():
     configuring_logging()
@@ -114,18 +110,14 @@ async def main():
     with open('negative_words.txt', encoding="utf-8") as file:
         charged_words = [word.replace('\n', '') for word in file.readlines()]
     title = ''
-    results = []
     async with aiohttp.ClientSession() as session:
-        # try:
-        async with anyio.create_task_group() as task_group:
-            for url in TEST_ARTICLES:
-                task_group.start_soon(process_article, session, morph, charged_words, url, title, results)
-        # except* Exception as excgroup:
-        #     for _ in excgroup.exceptions:
-        #         task_group.cancel_scope.cancel()
-
-    for result in results:
-        print(result)
+        try:
+            async with anyio.create_task_group() as task_group:
+                for url in TEST_ARTICLES:
+                    task_group.start_soon(process_article, session, morph, charged_words, url, title)
+        except* Exception as excgroup:
+            for _ in excgroup.exceptions:
+                task_group.cancel_scope.cancel()
 
 
 if __name__ == "__main__":
